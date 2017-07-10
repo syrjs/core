62717a1f1d473bb6f42d8f25c8fca3fab37e5930
Derek Anderson <dereanderson@paypal.com>
2017-07-10 14:29:29 -0700

    cleaned up source

--- samples/served.js
+++ samples/served.js
@@ -1,23 +1,40 @@
-
 let BaseComponent = require('../index.js')
 let RasterManager = require('../rastermanager.js')
 
 let basecomponent = new BaseComponent({
   style: {
-    left: 50,
     top: 50,
     height: 250,
     width: 250,
-    background: '#000000',
-    // background: '#6699ff',
+    backgroundColor: '#6699ff',
     display: 'block',
     position: 'absolute'
-  }
+  },
+  components: [
+    new BaseComponent({
+      type: 'Text',
+      content: 'Foo bar this ios a test',
+      style: {
+        backgroundColor: '#6699ff',
+        height: 10,
+        width: 250
+      }
+    }),
+    new BaseComponent({
+      type: 'Button',
+      content: 'Press This Button',
+      style: {
+        backgroundColor: '#000000',
+        color: '#ffffff',
+        top: 50,
+        height: 30,
+        width: 175
+      }
+    })
+  ]
 })
 
-
 if (window.webkit && window.webkit.messageHandlers) {
-
   // set the raster for the raster manager
   RasterManager.setRaster(require('../rasters/ast.js'))
   let ast = basecomponent.render()
