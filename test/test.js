778f7dec26eb52e4baa0d3b21d38a83e5fdd6d60
Derek Anderson <dereanderson@paypal.com>
2017-08-09 00:40:36 -0700

    coverage and tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,17 @@
 let assert = require('assert');
+import { Component, Render } from '../index';
 
 describe('Raster CLI', function() {
   describe('Base Component', function() {
     it('should render its base content', function() {
-      assert(0 == 0);
+        class MyComponent extends Component {
+          render() {
+            return <div>Hello World</div>
+          }
+        }
+
+        let mycomponent = new MyComponent();
+        assert.deepEqual(mycomponent.render() , { elementName: 'div', attributes: {}, children: [ 'Hello World' ] })
     });
   });
 });
