6869fd8703f61bfc8035776d8dad5f20136afd35
Derek Anderson <dereanderson@paypal.com>
2017-08-08 22:22:35 -0700

    pretty

--- samples/example.js
+++ samples/example.js
@@ -1,11 +1,9 @@
 import { Component, Render } from '../index';
 
 class MyComponent extends Component {
-    render() {
-      return (
-        <div foo="bar">Hello World</div>
-      )
-    }
+  render() {
+    return <div foo="bar">Hello World</div>;
+  }
 }
 
 Render(MyComponent);
--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ let assert = require('assert');
 describe('Raster CLI', function() {
   describe('Base Component', function() {
     it('should render its base content', function() {
-        assert(0 == 0);
+      assert(0 == 0);
     });
   });
 });
