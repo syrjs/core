8fa15fdd00c0cea404ab1a2df5761e0f4a934211
Derek Anderson <dereanderson@paypal.com>
2017-08-09 00:55:41 -0700

    updated tests, and package

--- test/test.js
+++ test/test.js
@@ -4,14 +4,18 @@ import { Component, Render } from '../index';
 describe('Raster CLI', function() {
   describe('Base Component', function() {
     it('should render its base content', function() {
-        class MyComponent extends Component {
-          render() {
-            return <div>Hello World</div>
-          }
+      class MyComponent extends Component {
+        render() {
+          return <div>Hello World</div>;
         }
+      }
 
-        let mycomponent = new MyComponent();
-        assert.deepEqual(mycomponent.render() , { elementName: 'div', attributes: {}, children: [ 'Hello World' ] })
+      let mycomponent = new MyComponent();
+      assert.deepEqual(mycomponent.render(), {
+        elementName: 'div',
+        attributes: {},
+        children: ['Hello World'],
+      });
     });
   });
 });
