3d7608ec28b616337d6531ecc706465a520267dd
Sherman Chen <=>
2017-11-14 14:37:23 -0800

    switch to babel-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['babel-preset-es2015'],
+          presets: ['@babel/env'],
           plugins: [['babel-plugin-transform-jsx', { useVariables: true }]],
         },
       },
