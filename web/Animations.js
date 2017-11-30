396cc940c5da99c6651d4ad6166903920c834f4b
Siddharth Malkireddy <smalkireddy@paypal.com>
2017-11-30 13:53:33 -0800

    XY animations are a go!!!!

--- web/Animations.js
+++ web/Animations.js
@@ -1,36 +1,58 @@
 import { Events } from '../lib/events';
 
 class animations {
-  animate(message) {
-      let animationValues = message.animation;
-      //using Web Animations API for now....will include a polyfill for unsupported browsers.
-      //this provides a clean structure for all the animations....doing rotate for the demo.
-      console.log(message.animation.animatedProperty);
-      let keyframes = [];
-      if(message.animation.animatedProperty) {
-      switch(message.animation.animatedProperty) {
-        case 'opacity': keyframes = [
+
+  animateInterpolate(message) {
+    let animationValues = message.animation;
+    let keyframes = [];
+    if(message.animation.animatedProperty) {
+    switch(message.animation.animatedProperty) {
+      case 'opacity': keyframes = [
                                     {opacity: animationValues.value},
                                     {opacity: animationValues.toValue}
-                                    ];
-                                    break;
-        //will need to include other cases for height,width, x, y transformations
-      }
-    } else {
-      keyframes = [
-          {transform: `rotate(${animationValues.value}deg)`},
-            {transform: `rotate(${animationValues.toValue}deg)`}
-          ]
+                                  ];
+                                  break;
+      //will need to include other cases for height,width, x, y transformations
     }
+  } else { //default to z animations A.K.A rotate
+    keyframes = [
+                  {transform: `rotate(${animationValues.value}deg)`},
+                  {transform: `rotate(${animationValues.toValue}deg)`}
+                ]
+  }
 
-      let animation = document.getElementById(message.guid)
-      .animate(keyframes, {
-            duration: animationValues.duration,
-            iterations: 1,
-          });
-      animation.onfinish = () => {
-        Events.emit({ type: 'animationComplete', guid: message.guid, animation: message.animation})
-      }
+    let animation = document.getElementById(message.guid)
+    .animate(keyframes, {
+          duration: animationValues.duration,
+          iterations: 1,
+        });
+    animation.onfinish = () => {
+      Events.emit({ type: 'animationComplete', guid: message.guid, animation: message.animation})
+    }
+  }
+
+  animateComponentXY(message) {
+    let animationValues = message.animation;
+    let animation = document.getElementById(message.guid)
+    .animate([
+                {transform: `translateX(${animationValues.x}px) translateY(${animationValues.y}px)`},
+                {transform: `translateX(${animationValues.x2}px) translateY(${animationValues.y2}px)`},
+      ], {
+          duration: animationValues.duration,
+          iterations: 1,
+        });
+    animation.onfinish = () => {
+      console.log('Animation Complete')
+      Events.emit({ type: 'animationComplete', guid: message.guid, animation: message.animation})
+    }
+  }
+
+  animate(message) {
+    console.log(message.animation);
+
+      (message.animation.value !== undefined)? this.animateInterpolate(message) : this.animateComponentXY(message);
+      //using Web Animations API for now....will include a polyfill for unsupported browsers.
+      //this provides a clean structure for all the animations....doing rotate for the demo.
   }
 
 }
