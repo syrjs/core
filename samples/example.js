c342356185ef685833c3ad95d5cde63f258bdea9
Siddharth Malkireddy <smalkireddy@paypal.com>
2017-11-30 17:56:33 -0800

    Pretty

--- samples/example.js
+++ samples/example.js
@@ -1,59 +1,60 @@
 import { Component, Render, View, Dimensions, Animated } from '../index';
 
 const styles = {
-  square : {
+  square: {
     width: 100,
     height: 100,
     backgroundColor: '#ff0000',
-    top: (Dimensions.get('window').height/2) - 50,
-    left: (Dimensions.get('window').width/2) - 50
-  }
-}
+    top: Dimensions.get('window').height / 2 - 50,
+    left: Dimensions.get('window').width / 2 - 50,
+  },
+};
 class MyComponent extends Component {
   constructor() {
     super();
     this.spinAnimation = new Animated.Value(0);
     this.opacityInAnimation = new Animated.Value(0);
     this.opacityOutAnimation = new Animated.Value(1);
-    this.moveAimation = new Animated.ValueXY({x: 0,y:600});
-    styles.square.transform = [ this.spinAnimation,
-                                {opacity: this.opacityInAnimation},
-                                {opacity: this.opacityOutAnimation},
-                                this.moveAimation
-                              ];
+    this.moveAimation = new Animated.ValueXY({ x: 0, y: 600 });
+    styles.square.transform = [
+      this.spinAnimation,
+      { opacity: this.opacityInAnimation },
+      { opacity: this.opacityOutAnimation },
+      this.moveAimation,
+    ];
   }
   render() {
-    return <Animated.View style={styles.square}></Animated.View>
+    return <Animated.View style={styles.square} />;
   }
-  moveUp(){
+  moveUp() {
     Animated.timing(this.moveAimation, {
-      toValue: ({x: 0,y:0}),
-      duration: 5000
+      toValue: { x: 0, y: 0 },
+      duration: 5000,
     }).start(() => {
       this.spin();
     });
   }
-  fadeIn(){
+  fadeIn() {
     Animated.timing(this.opacityInAnimation, {
       toValue: 1,
-      duration: 5000
-    }).start(()=>{
+      duration: 5000,
+    }).start(() => {
       this.fadeOut();
     });
   }
   fadeOut() {
     Animated.timing(this.opacityOutAnimation, {
       toValue: 0,
-      duration: 5000
-    }).start(()=>{
+      duration: 5000,
+    }).start(() => {
       this.fadeIn();
     });
   }
   spin() {
     Animated.timing(this.spinAnimation, {
       toValue: 360,
-      duration: 5000
-    }).start(()=>{
+      duration: 5000,
+    }).start(() => {
       this.spin();
     });
   }
