a2584d446af9762c673b945d116ec4de4ca76bc9
Sherman Chen <shc410@gmail.com>
2018-01-19 01:47:11 -0800

    refactor SyrImage.java (#138)

--- android/app/src/main/java/com/example/dereanderson/syrnativeandroid/SyrImage.java
+++ android/app/src/main/java/com/example/dereanderson/syrnativeandroid/SyrImage.java
@@ -1,6 +1,7 @@
 package com.example.dereanderson.syrnativeandroid;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -25,27 +26,50 @@ public class SyrImage implements SyrBaseModule {
         }
         String value = "";
         JSONObject style = null;
+        JSONObject source = null;
         Integer left = 0;
         Integer top = 0;
+
         try {
             //JSONObject instance = component.getJSONObject("instance");
             JSONObject attributes = component.getJSONObject("attributes");
 
-            if(attributes.has("style")) {
+            if (attributes.has("style")) {
                 style = attributes.getJSONObject("style");
                 imageView.setLayoutParams(SyrStyler.styleLayout(style));
                 SyrStyler.styleView(imageView, style);
+            }
+
+            if (attributes.has("source")) {
+                // grabs the source url if present
+                source = attributes.getJSONObject("source");
+                String path = source.toString();
+
+                // check if image exists in Resources
+                int pathExists = context.getResources().getIdentifier(path, "drawable", context.getPackageName());
+
+                if (pathExists != 0) {
+                    // if exist set image
+                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                    imageView.setImageResource(pathExists);
+                } else {
+                    // throw yellow box that image is not found in res/drawable
+                    // revert to a friendly image for now
+                    imageView.setImageResource(R.drawable.ic_muppets_misspiggy);
+                }
+
+            } else {
+                // throw red box because missing source prop entirely
             }
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        if(style != null) {
+        if (style != null) {
 
         }
-        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-        imageView.setImageResource(R.drawable.ic_muppets_misspiggy);
+
         return imageView;
     }
 
