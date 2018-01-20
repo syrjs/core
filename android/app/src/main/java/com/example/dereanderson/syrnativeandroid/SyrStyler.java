bc225dde6abcbbd939b67dc63d235c1a047d9420
Derek Anderson <dereanderson@paypal.com>
2018-01-20 00:17:19 -0800

    lands rgba support in android

--- android/app/src/main/java/com/example/dereanderson/syrnativeandroid/SyrStyler.java
+++ android/app/src/main/java/com/example/dereanderson/syrnativeandroid/SyrStyler.java
@@ -12,11 +12,43 @@ import android.widget.RelativeLayout;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by dereanderson on 1/9/18.
  */
 
 public class SyrStyler{
+
+    static public int getColor(String colorString) {
+        if(colorString.contains("rgb")) {
+            if(colorString.contains("rgba")) {
+                colorString = colorString.replace("rgba(","");
+            }
+            colorString = colorString.replace(("rgb"), "");
+            colorString = colorString.replace(")", "");
+
+            String[] colors = colorString.split(",");
+
+            int red = Integer.parseInt(colors[0]);
+            int blue = Integer.parseInt(colors[1]);
+            int green = Integer.parseInt(colors[2]);
+
+            double alphaD = 0.00;
+            if(colors[3] != null){
+                alphaD = Double.parseDouble(colors[3]);
+                alphaD = 255/alphaD;
+            }
+            
+            int alpha = (int)alphaD;
+
+            return Color.argb(175, red, blue, green);
+        } else if(colorString.contains("#")) {
+            return Color.parseColor(colorString);
+        }
+        return Color.parseColor("#ffffff");
+    }
+
     static public ViewGroup.LayoutParams styleLayout(JSONObject style) {
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(0,0);
         try {
@@ -50,12 +82,11 @@ public class SyrStyler{
 
         if(style.has("backgroundColor")) {
             try {
-
                 String backgroundColor = style.getString("backgroundColor");
 
                 GradientDrawable gd = new GradientDrawable(
                         GradientDrawable.Orientation.TOP_BOTTOM,
-                        new int[] {Color.parseColor(backgroundColor), Color.parseColor(backgroundColor)});
+                        new int[] {getColor(backgroundColor), getColor(backgroundColor)});
 
                 if(style.has("borderRadius")) {
                     Integer borderRadius = style.getInt("borderRadius");
@@ -64,11 +95,11 @@ public class SyrStyler{
 
                 if(style.has("borderColor") && style.has("borderWidth")) {
                     // borders on views
-                    gd.setStroke(style.getInt("borderWidth"), Color.parseColor(style.getString("borderColor")));
+                    gd.setStroke(style.getInt("borderWidth"), getColor(style.getString("borderColor")));
 
                 } else  if(style.has("borderColor")) {
 
-                    gd.setStroke(3, Color.parseColor(style.getString("borderColor")));
+                    gd.setStroke(3, getColor(style.getString("borderColor")));
                 }
 
                 component.setBackground(gd);
