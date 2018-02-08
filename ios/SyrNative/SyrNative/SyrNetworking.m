a48a4e6ad4191dd608db3e8165c154e2da309eb2
Derek Anderson <dereanderson@paypal.com>
2018-02-07 23:29:26 -0800

    add minor network calls for fetch POST/GET to walk around CORs in WKWebView

--- ios/SyrNative/SyrNative/SyrNetworking.m
+++ ios/SyrNative/SyrNative/SyrNetworking.m
@@ -12,7 +12,7 @@
 
 SYR_EXPORT_MODULE()
 
-SYR_EXPORT_METHOD(get: (NSDictionary*) requestDict) {
+SYR_EXPORT_METHOD(request: (NSDictionary*) requestDict) {
   // fetch on the async queue
   dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul);
   dispatch_async(queue, ^{
@@ -21,6 +21,21 @@ SYR_EXPORT_METHOD(get: (NSDictionary*) requestDict) {
     NSString* guid = [requestDict valueForKey:@"guid"];
     NSString* method = [requestDict valueForKey:@"method"];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    NSString* body = [requestDict valueForKey:@"body"];
+    id headers = [requestDict objectForKey:@"headers"];
+    
+    if(body != nil) {
+      NSData* postData = [body dataUsingEncoding:NSUTF8StringEncoding];
+      NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+      [request setHTTPBody: postData];
+      [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    }
+
+    // set more headers here
+    for (id key in headers) {
+      [request setValue:[headers valueForKey:key]  forHTTPHeaderField:key];
+    }
+    
     [request setHTTPMethod:method];
     [request setURL:[NSURL URLWithString:requestUrl]];
     
@@ -30,7 +45,6 @@ SYR_EXPORT_METHOD(get: (NSDictionary*) requestDict) {
     // get data
     NSData *oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseCode error:&error];
     
-    
     // log error if not success just for info
     if([responseCode statusCode] != 200){
       NSLog(@"Error getting %@, HTTP status code %li", requestUrl, (long)[responseCode statusCode]);
@@ -39,7 +53,6 @@ SYR_EXPORT_METHOD(get: (NSDictionary*) requestDict) {
     // get the response
     NSString* response = [[NSString alloc] initWithData:oResponseData encoding:NSASCIIStringEncoding];
     NSNumber* statusCode = [NSNumber numberWithDouble:[responseCode statusCode]];
-    
     
     // send the response back
     [self sendEventWithName:@"NetworkingCallback" body:@{@"data": response, @"responseCode": statusCode, @"guid": guid}];
