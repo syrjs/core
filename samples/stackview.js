f774a540841691a243e6d953f8fa5062c8ad39bd
MSiddharthReddy <siddharthreddy.malkireddy@sjsu.edu>
2018-02-16 22:20:56 -0800

    Updates (#165)
    * Included a polyfill for web-animations api. Its a CDN to the standard web-aniamtions polyfill
    
    * Fixes for Web Animator. Translations
    
    * Including height animations  and fixing XY animations
    
    * Inlcuded width animations. Ran Pretty and Cleaned up
    
    * Fixed XY animations to respect the changes
    
    * Central Flux Store for SYR
    
    * Exposing SyrStore from syr
    
    * Removing Current Frame animation to reproduce smoother transition
    
    * Fixing auto height in stackView
    
    * Minor syntax Changes
    
    * lib/rastermanager.js
    
    * Fixing Array of children with state changes
    
    * Calculating updated children
    
    * Calculating updated children
    
    * creating the new children
    
    *  rendering new children
    
    * Remove console logs and keeping the updates in order
    
    * Finally fixing the animations
    
    * Commiting Delete changes thus far

--- samples/stackview.js
+++ samples/stackview.js
@@ -16,10 +16,22 @@ import {
 } from '../index';
 
 class OtherClass extends Component {
+  constructor(){
+    super();
+    this.state.color = '#ff00ff'
+  }
   render() {
+    let self = this;
     return (
-      <TouchableOpacity onPress={()=>{this.onPressHandler('+')}} style={{height:50, width:100}}>
-          <View style={{height:50, width:100, backgroundColor:'#ff00ff'}}></View>
+      <TouchableOpacity onPress={ () => {
+        //console.log('My key', this.props.key, this.uuid);
+        this.setState({
+          color: '#000000'
+        }, () => {
+          //console.log('Changed State :', this.state)
+        })
+    }} style={{height:50, width:100}}>
+          <View style={{height:50, width:100, backgroundColor: this.state.color}}></View>
       </TouchableOpacity>
     )
   }
@@ -28,22 +40,34 @@ class OtherClass extends Component {
 
 
 class example extends Component {
+  constructor() {
+    super();
+    this.state.array= [1];
+  }
+  componentDidMount() {
+    this.setState({
+      array: [1,2]
+    })
+  }
   render() {
     return (
       <StackView
       axis="vertical"
-      style={{height:'auto', width:300}}>
-       <Button onPress={()=>{this.onPressHandler('+')}} style={{height:50, width:100}}>button</Button>
-       <Button onPress={()=>{this.onPressHandler('+')}} style={{height:50, width:100}}>button</Button>
-       <Button onPress={()=>{this.onPressHandler('+')}} style={{height:50, width:100}}>button</Button>
-       <Button onPress={()=>{this.onPressHandler('+')}} style={{height:50, width:100}}>button</Button>
-        <Button onPress={()=>{this.onPressHandler('+')}} style={{height:50, width:100}}>button</Button>
-        <OtherClass></OtherClass>
+      style={{height:'auto', width:300}}
+      spacing="100"
+      >
+      {this.returnClass()}
       </StackView>
     );
   }
+
+  returnClass() {
+    return this.state.array.map((a) => {
+      return  <OtherClass key={a}></OtherClass>
+    })
+  }
   onPressHandler(btn) {
-    console.log('click');
+    //console.log('click');
   }
 }
 
