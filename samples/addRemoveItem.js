2d5ed9e552679adfb287a8ece71c22fbd93aa67c
Derek <dmikey@users.noreply.github.com>
2018-02-20 13:48:28 -0800

    Addition Remove Right Now (#169)
    * working on remove and add
    
    * does simple additions to the tree

---
+++ samples/addRemoveItem.js
@@ -0,0 +1,45 @@
+import {
+  Component,
+  Render,
+  View
+} from '../index';
+
+class MyView extends Component {
+  render() {
+    return <View style={{width:50, height:50, top:(this.props.key*55), backgroundColor:'#000000'}}/>
+  }
+  componentDidMount() {
+    console.log(this.props.key);
+  }
+}
+
+function getItems(n) {
+  let components = [];
+  for(let i=0; i<=(n-1); i++) {
+    components.push(<MyView key={i}/>);
+  }
+  return components;
+}
+
+class example extends Component {
+  constructor(props) {
+    super(props);
+    this.state.components = getItems(1);
+  }
+  render() {
+    return (
+        <View style={{width:100, height:300, backgroundColor:'#ff00ff'}}>
+          { this.state.components }
+        </View>
+    );
+  }
+  componentDidMount() {
+    console.log('setting state');
+
+    this.setState({
+      components: getItems(5)
+    })
+  }
+}
+
+Render(example);
