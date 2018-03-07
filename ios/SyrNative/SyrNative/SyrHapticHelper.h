a8a596feaf63fc5f9e874816c4f62f5bd5738199
msiddharthreddy <msiddharthreddy@github.com>
2018-03-07 15:11:17 -0800

    Removing enums

--- ios/SyrNative/SyrNative/SyrHapticHelper.h
+++ ios/SyrNative/SyrNative/SyrHapticHelper.h
@@ -11,16 +11,6 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-typedef enum {
-    FeedbackType_Selection,
-    FeedbackType_Impact_Light,
-    FeedbackType_Impact_Medium,
-    FeedbackType_Impact_Heavy,
-    FeedbackType_Notification_Success,
-    FeedbackType_Notification_Warning,
-    FeedbackType_Notification_Error
-}FeedbackType;
-
 #define getEnumValue(name) \
 [NSNumber numberWithInt:#name]
 
