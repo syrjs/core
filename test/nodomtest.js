2be11f365b996a3ec16675442de4fb714dff8b67
Derek Anderson <dereanderson@paypal.com>
2018-03-27 12:03:23 -0700

    adds no-dom raster for tests

---
+++ test/nodomtest.js
@@ -0,0 +1,30 @@
+import { NoDOM } from '../lib/rasters/nodom';
+
+const assert = require('assert');
+
+describe('NoDOM', function() {
+  describe('render', function() {
+    it('pass a message to render', function() {
+      NoDOM.recieveMessage = (type, message) => {
+        assert.deepEqual(message, {foo:'bar'});
+      }
+      NoDOM.render({
+        foo: 'bar'
+      })
+    });
+  });
+  describe('dimensions', function() {
+    it('pass back dimensions provided', function() {
+      NoDOM.dimensionValues = {
+        height: 100,
+        width: 100,
+        scale: 100
+      };
+      assert.deepEqual(NoDOM.dimensions(), {
+        height: 100,
+        width: 100,
+        scale: 100
+      });
+    });
+  });
+});
