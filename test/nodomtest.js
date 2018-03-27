d2b6bc7527489cc1e7891ebe428716ad4f467048
Derek Anderson <dereanderson@paypal.com>
2018-03-27 12:07:11 -0700

    pretty

--- test/nodomtest.js
+++ test/nodomtest.js
@@ -6,11 +6,11 @@ describe('NoDOM', function() {
   describe('render', function() {
     it('pass a message to render', function() {
       NoDOM.recieveMessage = (type, message) => {
-        assert.deepEqual(message, {foo:'bar'});
-      }
+        assert.deepEqual(message, { foo: 'bar' });
+      };
       NoDOM.render({
-        foo: 'bar'
-      })
+        foo: 'bar',
+      });
     });
   });
   describe('dimensions', function() {
@@ -18,12 +18,12 @@ describe('NoDOM', function() {
       NoDOM.dimensionValues = {
         height: 100,
         width: 100,
-        scale: 100
+        scale: 100,
       };
       assert.deepEqual(NoDOM.dimensions(), {
         height: 100,
         width: 100,
-        scale: 100
+        scale: 100,
       });
     });
   });
