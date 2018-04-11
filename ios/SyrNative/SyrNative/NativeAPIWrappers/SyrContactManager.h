916564b701e9577fd6a4c51cfde8035a47db6119
Hassan Syed <hassyed@paypalcorp.com>
2018-04-11 16:22:18 -0700

    Updated the documents. Added a wrapper for iOS Contacts API
    Moved the API related things to it's own folder:
    
    Changed the name of the folder

---
+++ ios/SyrNative/SyrNative/NativeAPIWrappers/SyrContactManager.h
@@ -0,0 +1,13 @@
+//
+//  SyrContactManager.h
+//  SyrNative
+//
+//  Created by Syed,hassan on 4/11/18.
+//  Copyright © 2018 Anderson,Derek. All rights reserved.
+//
+
+#import <SyrNative/SyrNative.h>
+
+@interface SyrContactManager : SyrComponent
++(instancetype)sharedInstance;
+@end
