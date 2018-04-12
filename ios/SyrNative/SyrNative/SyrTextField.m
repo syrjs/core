4c6e6f2e06756bf2dc7afdf2643a1704e8a63a6b
Hassan Syed <hassyed@paypalcorp.com>
2018-04-12 10:29:00 -0700

    Added a textField and handling for 4 of its delegates

---
+++ ios/SyrNative/SyrNative/SyrTextField.m
@@ -0,0 +1,63 @@
+//
+//  SyrTextField.m
+//  SyrNative
+//
+//  Created by Syed,hassan on 4/12/18.
+//  Copyright © 2018 Anderson,Derek. All rights reserved.
+//
+
+#import "SyrTextField.h"
+
+@implementation SyrTextField
++(instancetype)sharedInstance {
+    static SyrTextField *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+
++(NSObject *)render:(NSDictionary *)component withInstance:(NSObject *)componentInstance {
+    
+    NSDictionary* style = [[component objectForKey:@"instance"] valueForKey:@"style"];
+    
+    UITextField *textField = [[[UITextField alloc]  init] initWithFrame:[SyrStyler styleFrame:style]];
+    
+    textField.delegate = SyrTextField.sharedInstance;
+    
+    textField.textColor = [SyrStyler colorFromHash:[style objectForKey:@"color"]];
+    
+    textField.placeholder = [[component objectForKey:@"props"] objectForKey:@"placeholder"];
+    
+    return [SyrStyler styleView:textField withStyle:style];
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField reason:(UITextFieldDidEndEditingReason)reason {
+    
+    if(reason == UITextFieldDidEndEditingReasonCommitted) {
+        [SyrTextField sendEventWithName:@"TextFieldDidFinishEditing" body:@{@"text":textField.text,@"reason":@"committed"}];
+    } else {
+        [SyrTextField sendEventWithName:@"TextFieldDidFinishEditing" body:@{@"text":textField.text,@"reason":@"cancelled"}];
+    }
+    
+}
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField {
+    [SyrTextField sendEventWithName:@"TextFieldBeganEditing" body:@{}];
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [SyrTextField sendEventWithName:@"TextFieldProcessingReturn" body:@{@"text":textField.text}];
+
+    return true;
+}
+
+-(BOOL)textFieldShouldClear:(UITextField *)textField {
+    [SyrTextField sendEventWithName:@"TextFieldClearingText" body:@{@"text":textField.text}];
+
+    return true;
+}
+
+
+@end
