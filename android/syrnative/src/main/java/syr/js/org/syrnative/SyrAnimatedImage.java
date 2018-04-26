3cb9947732bacbbd3c81f0bc6ca69a1361da7137
msiddharthreddy <msiddharthreddy@github.com>
2018-04-26 12:11:59 -0700

    Clean up animatedImage and animatedView

--- android/syrnative/src/main/java/syr/js/org/syrnative/SyrAnimatedImage.java
+++ android/syrnative/src/main/java/syr/js/org/syrnative/SyrAnimatedImage.java
@@ -16,72 +16,9 @@ public class SyrAnimatedImage extends SyrImage {
     @Override
     public View render(JSONObject component, Context context, View instance) {
 
-        ImageView imageView;
-        if(instance != null) {
-            imageView = (ImageView) instance;
-        } else {
-            imageView = new ImageView(context);
-        }
-        String value = "";
-        JSONObject style = null;
-        JSONObject source = null;
-        Integer left = 0;
-        Integer top = 0;
-
-        imageView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-
-        try {
-            JSONObject jsonInstance = component.getJSONObject("instance");
-            JSONObject jsonProps = jsonInstance.getJSONObject("props");
-
-            if (jsonInstance.has("style")) {
-                style = jsonInstance.getJSONObject("style");
-                imageView.setLayoutParams(SyrStyler.styleLayout(style));
-                SyrStyler.styleView(imageView, style);
-
-
-                if(style.has("left")) {
-                    imageView.setX(style.getInt("left"));
-                }
-
-                if(style.has("top")) {
-                    imageView.setY(style.getInt("top"));
-                }
-
-            }
-
-            if (jsonProps.has("source")) {
-                // grabs the source url if present
-                source = jsonProps.getJSONObject("source");
-                String path = source.getString("uri");
-
-                // check if image exists in Resources
-                int pathExists = context.getResources().getIdentifier(path, "drawable", context.getPackageName());
-
-                if (pathExists != 0) {
-                    // if exist set image
-                    imageView.setImageResource(pathExists);
-                } else {
-                    // throw yellow box that image is not found in res/drawable
-                    // revert to a friendly image for now
-                    imageView.setBackgroundColor(Color.parseColor("#ff00ff"));
-                }
-
-                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-
-            } else {
-                // throw red box because missing source prop entirely
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        if (style != null) {
-
-        }
-
-        return imageView;
+        View animatedImage = super.render(component, context, instance);
+        animatedImage.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        return animatedImage;
     }
     @Override
     public String getName() {
--- android/syrnative/src/main/java/syr/js/org/syrnative/SyrAnimatedView.java
+++ android/syrnative/src/main/java/syr/js/org/syrnative/SyrAnimatedView.java
@@ -16,54 +16,9 @@ public class SyrAnimatedView extends SyrView {
     @Override
     public View render(JSONObject component, Context context, View instance) {
 
-        RelativeLayout layout;
-        if(instance != null) {
-            layout = (RelativeLayout) instance;
-        } else {
-            layout = new RelativeLayout(context);
-        }
-
-        // word on the street is this should only be used for animations?
-        // these are not free
-        layout.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-
-        JSONObject style = null;
-        try {
-            JSONObject componentInstance = component.getJSONObject("instance");
-            if(componentInstance.has("style")){
-                style = componentInstance.getJSONObject("style");
-                layout.setLayoutParams(SyrStyler.styleLayout(style));
-
-                if(style.has("left")) {
-                    layout.setX(style.getInt("left"));
-                }
-
-                if(style.has("top")) {
-                    layout.setY(style.getInt("top"));
-                }
-
-                if(style.has("opacity")) {
-                    layout.setAlpha(style.getInt("opacity"));
-                }
-
-                SyrStyler.styleView(layout, style);
-
-                if(style.has("overflow")) {
-                    String overflow = style.getString("overflow");
-                    if(overflow.contains("hidden")) {
-                        layout.setClipChildren(true);
-                    } else {
-                        layout.setClipChildren(false);
-                    }
-                }
-
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        return layout;
+        View animatedView = super.render(component, context, instance);
+        animatedView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        return animatedView;
     }
     @Override
     public String getName() {
