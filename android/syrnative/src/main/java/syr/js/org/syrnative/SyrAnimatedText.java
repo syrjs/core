d17bc4c58d66369cdd4a8213c5b28f28748b9279
msiddharthreddy <msiddharthreddy@github.com>
2018-04-26 19:32:50 -0700

    Animated Text fixex

--- android/syrnative/src/main/java/syr/js/org/syrnative/SyrAnimatedText.java
+++ android/syrnative/src/main/java/syr/js/org/syrnative/SyrAnimatedText.java
@@ -1,10 +1,23 @@
 package syr.js.org.syrnative;
 
+import android.content.Context;
+import android.view.View;
+
+import org.json.JSONObject;
+
 /**
  * Created by dereanderson on 1/16/18.
  */
 
 public class SyrAnimatedText extends SyrText {
+
+    @Override
+    public View render(JSONObject component, Context context, View instance) {
+        View animatedText = super.render(component, context, instance);
+        animatedText.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        return animatedText;
+    }
+
     @Override
     public String getName() {
         return "AnimatedText";
