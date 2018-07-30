7706c074d4c1216824b7351ec0f199a3feb5dd37
Derek Anderson <dereanderson@paypal.com>
2018-07-30 16:44:41 -0700

    added a bunch of goodies

---
+++ samples/nullRender.js
@@ -0,0 +1,32 @@
+import {
+  Component,
+  Render,
+  View,
+  Button,
+  PixelRatio
+} from '../index';
+
+class example extends Component {
+  constructor(props) {
+    super(props);
+    this.state.doRender = false;
+  }
+  render() {
+    return null;
+  }
+  componentDidMount() {
+    console.log('component did mount');
+  }
+  handleOnRemove() {
+    this.setState({
+      doRender: false
+    });
+  }
+  handleOnAdd() {
+    this.setState({
+      doRender: true
+    });
+  }
+}
+
+Render(example);
