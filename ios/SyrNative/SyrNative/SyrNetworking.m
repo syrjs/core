360fb586b499e2db5019a49967c99883fd88576c
Chris Brody <chris.brody@gmail.com>
2018-08-30 12:51:21 -0400

    Minor SyrNetworking.m cleanup fixes (#263)
    - @TODO comments
    - split up extremely long statements
    - indentation fixes
    - remove trailing spaces

--- ios/SyrNative/SyrNative/SyrNetworking.m
+++ ios/SyrNative/SyrNative/SyrNetworking.m
@@ -15,6 +15,7 @@ SYR_EXPORT_MODULE()
 SYR_EXPORT_METHOD(request: (NSDictionary*) requestDict) {
   // fetch on the async queue
   dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul);
+
   dispatch_async(queue, ^{
     // get request
     NSString* requestUrl = [requestDict valueForKey:@"url"];
@@ -22,9 +23,9 @@ SYR_EXPORT_METHOD(request: (NSDictionary*) requestDict) {
     NSString* method = [requestDict valueForKey:@"method"];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
     NSString* body = [requestDict valueForKey:@"body"];
-    
+
     id headers = [requestDict objectForKey:@"headers"];
-    
+
     if(body != nil) {
       NSData* postData = [body dataUsingEncoding:NSUTF8StringEncoding];
       NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
@@ -36,35 +37,36 @@ SYR_EXPORT_METHOD(request: (NSDictionary*) requestDict) {
     for (id key in headers) {
       [request setValue:[headers valueForKey:key]  forHTTPHeaderField:key];
     }
-    
+
     [request setHTTPMethod:method];
     [request setURL:[NSURL URLWithString:requestUrl]];
-    
-    // todo: forward these errors along with the data response
+
+    // @TODO: forward these errors along with the data response
     [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * data, NSURLResponse * response, NSError * error) {
       NSDictionary* platformError = nil;
       NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *) response;
-      
+
       // log error if not success just for info
       if([httpResponse statusCode]!= 200){
         NSLog(@"Error getting %@, HTTP status code %li", requestUrl, (long)[httpResponse statusCode]);
       }
-      
+
       if(error != nil) {
         platformError = @{@"message": [error localizedDescription]};
       } else {
         platformError = @{};
       }
-      
+
       // get the response
-   		NSString* responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+      NSString* responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
       NSNumber* statusCode = [NSNumber numberWithDouble:[httpResponse statusCode]];
-      
+
       // send the response back
-      [self sendEventWithName:@"NetworkingCallback" body:@{@"data": responseString, @"responseCode": statusCode, @"guid": guid, @"platformError":platformError}];
-      
+      [self sendEventWithName:@"NetworkingCallback"
+          body:@{@"data": responseString, @"responseCode": statusCode, @"guid": guid, @"platformError":platformError}];
+
     }] resume];
-    
+
   });
 }
 
