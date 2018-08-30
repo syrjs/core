19e9b9706725a33f798e9ee1acc78cbd61729b10
Chris Brody <chris.brody@gmail.com>
2018-08-29 23:36:39 -0400

    Cleanup iOS bridge (#262)
    * remove extra WKWebView alloc from iOS bridge
    
    * cleanup iOS bridge init
    
    * Cleanup some iOS bridge functions
    
    * Remove initWithRootView method from iOS SyrBridge
    
    * Fix @TODO comments in iOS SyrBridge
    
    * Other comment cleanup in iOS SyrBridge.m
    
    * Remove trailing spaces from iOS SyrBridge.m

--- ios/SyrNative/SyrNative/SyrBridge.m
+++ ios/SyrNative/SyrNative/SyrBridge.m
@@ -25,31 +25,25 @@
 - (id) init
 {
   self = [super init];
+
   if (self!=nil) {
-    _bridgedBrowser = [[WKWebView alloc] init];
-    // setup a 0,0,0,0 wkwebview to use the jsbridge
-    WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
+    // js bridge configuration
     WKUserContentController *controller = [[WKUserContentController alloc] init];
-    // create a js bridge
     [controller addScriptMessageHandler:self name:@"SyrNative"];
+    WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
     configuration.userContentController = controller;
+
+    // setup a 0,0,0,0 wkwebview to use the js bridge
     _bridgedBrowser = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, 0, 0) configuration:configuration];
     _bridgedBrowser.navigationDelegate = self;
+
+    // connect the bridge to other components
     _eventHandler = [SyrEventHandler sharedInstance];
     _eventHandler.bridge = self;
     _raster = [SyrRaster sharedInstance];
     _raster.bridge = self;
   }
-  return self;
-}
 
-- (id) initWithRootView: (SyrRootView*) rootView {
-  self = [super init];
-  if (self)
-  {
-    self.rootView = rootView;
-    [self addView];
-  }
   return self;
 }
 
@@ -67,16 +61,16 @@
 /**
  load the javascript bundle.
  we use an html fixture to aid in loading
- 
+
  we pass as query the native modules available
  and envrionment info
  */
-- (void) loadBundle: (NSString*) withBundlePath withRootView: (SyrRootView*) rootView{
-  
+- (void) loadBundle: (NSString*) withBundlePath withRootView: (SyrRootView*) rootView
+{
   // load a bundle with the root view we were handed
-  // todo multiplex bridge : multiple apps, one instance
+  // @TODO multiplex bridge : multiple apps, one instance
    _rootView = rootView;
-  
+
   NSURL* syrBridgeUrl;
   if([withBundlePath containsString:@"http"]) {
     syrBridgeUrl = [NSURL URLWithString:withBundlePath];
@@ -87,19 +81,19 @@
 
   NSURLComponents *components = [NSURLComponents componentsWithURL:syrBridgeUrl resolvingAgainstBaseURL:YES];
   NSMutableArray* exportedMethods = [[NSMutableArray alloc] init];
-  
+
   // pass native module names and selectors to the javascript side
   NSMutableArray *queryItems = [NSMutableArray array];
   for (NSString *key in _raster.nativemodules) {
     NSString *new = [key stringByReplacingOccurrencesOfString: @"__syr_export__" withString:@"_"];
     [exportedMethods addObject:new];
   }
-  
+
   // setup some environment stuff for the interpreter
   NSNumber* width = [NSNumber numberWithDouble:[UIScreen mainScreen].bounds.size.width];
   NSNumber* height = [NSNumber numberWithDouble:[UIScreen mainScreen].bounds.size.height];
   NSDictionary* bootupProps = [rootView appProperties];
-  
+
   NSData *jsonData = [NSJSONSerialization dataWithJSONObject:bootupProps
                                                      options:NSJSONWritingPrettyPrinted
                                                        error:nil];
@@ -107,13 +101,13 @@
   NSData *jsonExportedMethodsData = [NSJSONSerialization dataWithJSONObject:exportedMethods
                                                      options:NSJSONWritingPrettyPrinted
                                                        error:nil];
-  
+
   NSString* uriStringExportedMethods = [[NSString alloc] initWithData:jsonExportedMethodsData encoding:NSUTF8StringEncoding];
   NSString* uriStringBootupProps = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-  
+
   CGFloat screenScale = [[UIScreen mainScreen] scale];
   NSNumber* screenScaleNS = [NSNumber numberWithFloat:screenScale];
-  
+
   [queryItems addObject:[NSURLQueryItem queryItemWithName:@"initial_props" value:uriStringBootupProps]];
   [queryItems addObject:[NSURLQueryItem queryItemWithName:@"window_width" value:[width stringValue]]];
   [queryItems addObject:[NSURLQueryItem queryItemWithName:@"window_height" value:[height stringValue]]];
@@ -122,11 +116,12 @@
   [queryItems addObject:[NSURLQueryItem queryItemWithName:@"platform_version" value:[[UIDevice currentDevice] systemVersion]]];
   [queryItems addObject:[NSURLQueryItem queryItemWithName:@"exported_methods" value:uriStringExportedMethods]];
   [queryItems addObject:[NSURLQueryItem queryItemWithName:@"model" value:[self deviceName]]];
-  
+
   components.queryItems = queryItems;
   NSURLRequest * req = [NSURLRequest requestWithURL:components.URL];
-  [_bridgedBrowser loadRequest:req]; //[_bridgedBrowser loadFileURL:components.URL allowingReadAccessToURL:components.URL];
-  
+  // [_bridgedBrowser loadFileURL:components.URL allowingReadAccessToURL:components.URL];
+  [_bridgedBrowser loadRequest:req];
+
   [NSTimer scheduledTimerWithTimeInterval:2.0
                                    target:self
                                  selector:@selector(heartBeat)
@@ -134,19 +129,20 @@
                                   repeats:YES];
 }
 
--(NSString*) resourceBundlePath{
+-(NSString*) resourceBundlePath
+{
   return _rootView.resourceBundlePath;
 }
 
 - (void) heartBeat {
   NSString* js = [NSString stringWithFormat:@""];
-  
+
   // dispatching on the bridge to wkwebview needs to be done on the main thread
   dispatch_async(dispatch_get_main_queue(), ^{
     [self->_bridgedBrowser evaluateJavaScript:js completionHandler:^(id result, NSError *error) {
       if (error == nil)
       {
-        // do something with JS returns here
+        // @TODO do something with JS returns here
       }
       else
       {
@@ -157,26 +153,26 @@
 }
 
 /**
-	the bridge sending a message for us to act on
+ the bridge sending a message for us to act on
  */
 - (void)userContentController:(WKUserContentController *)userContentController
       didReceiveScriptMessage:(WKScriptMessage *)message {
-  
+
   NSDictionary* syrMessage = [message valueForKey:@"body"];
   NSString* messageType = [syrMessage valueForKey:@"type"];
   if([messageType containsString:@"cmd"]) {
-    
+
     // keep messaging on the async queue
     [self invokeMethodWithMessage:syrMessage];
   } else if([messageType containsString:@"gui"]) {
-    
-    //updating the UI needs to be done on the main thread
+
+    // updating the UI needs to be done on the main thread
     dispatch_async(dispatch_get_main_queue(), ^{
       [self->_raster parseAST:syrMessage withRootView:self->_rootView];
     });
-   
+
   } else if([messageType containsString:@"animation"]) {
-    
+
     // animations define the thread they are on
     [_raster setupAnimation:syrMessage];
   } else if([messageType containsString:@"error"]) {
@@ -188,37 +184,38 @@
  Invoke a class method from the signature we are given.
  assume the data types, and use NSObject to pass them through
  */
-- (void)invokeMethodWithMessage: (NSDictionary*) syrMessage {
-  
+- (void)invokeMethodWithMessage: (NSDictionary*) syrMessage
+{
   NSData *objectData = [[syrMessage valueForKey:@"ast"] dataUsingEncoding:NSUTF8StringEncoding];
   NSDictionary *astDict = [NSJSONSerialization JSONObjectWithData:objectData
                                                           options:NSJSONReadingMutableContainers
                                                             error:nil];
-  
+
   // get the class
   NSString* className = [_raster.registeredClasses valueForKey:[astDict valueForKey:@"clazz"]];
   Class class = NSClassFromString(className);
-  
+
   // create an instance of the object
   if(class != nil){
     [_instances setObject:class forKey:className];
   }
-  
-  //get render method
+
+  // get render method
   NSString* selectorString = [NSString stringWithFormat:@"__syr_export__%@", [astDict valueForKey:@"method"]];
   SEL methodSelector = NSSelectorFromString(selectorString);
   if ([class respondsToSelector:methodSelector]) {
-    
+
     NSMethodSignature *methodSignature = [NSClassFromString(className) methodSignatureForSelector:methodSelector];
     //invoke render method, pass component
     NSInvocation *inv = [NSInvocation invocationWithMethodSignature:methodSignature];
-    
+
     [inv setSelector:methodSelector];
     [inv setTarget:class];
-    
+
     NSData *argsData = [[astDict valueForKey:@"args"] dataUsingEncoding:NSUTF8StringEncoding];
     NSError *error;
-    //    Note that JSONObjectWithData will return either an NSDictionary or an NSArray, depending whether your JSON string represents an a dictionary or an array.
+    // Note that JSONObjectWithData will return either an NSDictionary or an NSArray,
+    // depending whether your JSON string represents an a dictionary or an array.
     id argsObject = [NSJSONSerialization JSONObjectWithData:argsData options:0 error:&error];
     int argsIndex = 2; // start at 2
     for(id arg in argsObject) {
@@ -232,11 +229,12 @@
 
 /**
  if the page is refreshed, we need to thrash the render
- todo - ensure this isn't leaking
+ @TODO - ensure this isn't leaking
  */
 - (void)webView:(WKWebView *)webView
-didStartProvisionalNavigation:(WKNavigation *)navigation {
-  NSLog(@"Loading Bundle");
+        didStartProvisionalNavigation:(WKNavigation *)navigation
+{
+  NSLog(@"Reloading Bundle");
   // bundle reloaded, remove all subviews from root view
   [_raster reset];
   [_rootView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
@@ -244,7 +242,7 @@ didStartProvisionalNavigation:(WKNavigation *)navigation {
 
 /**
  send an event to through the bridge to the javascript side.
- todo - handle errors
+ @TODO - handle errors
  */
 - (void) sendEvent: (NSDictionary*) message {
   // send events on an async queue
@@ -255,7 +253,7 @@ didStartProvisionalNavigation:(WKNavigation *)navigation {
                                                             error:nil];
     NSString *messageString = [[NSString alloc] initWithData:messageData encoding:NSUTF8StringEncoding];
     NSString* js = [NSString stringWithFormat:@"SyrEvents.emit(%@)", messageString];
-    
+
     // dispatching on the bridge to wkwebview needs to be done on the main thread
     dispatch_async(dispatch_get_main_queue(), ^{
       [self->_bridgedBrowser evaluateJavaScript:js completionHandler:^(id result, NSError *error) {
@@ -273,8 +271,9 @@ didStartProvisionalNavigation:(WKNavigation *)navigation {
 }
 
 - (void)webView:(WKWebView *)webView
-didFailProvisionalNavigation:(WKNavigation *)navigation
-      withError:(NSError *)error {
+        didFailProvisionalNavigation:(WKNavigation *)navigation
+        withError:(NSError *)error
+{
   NSLog(@"error");
 }
 
@@ -295,7 +294,7 @@ didFailProvisionalNavigation:(WKNavigation *)navigation
 {
     struct utsname systemInfo;
     uname(&systemInfo);
-    
+
     return [NSString stringWithCString:systemInfo.machine
                               encoding:NSUTF8StringEncoding];
 }
