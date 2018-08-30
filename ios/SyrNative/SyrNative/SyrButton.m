7ce42661433c7f1fc0b0156aaab09d3f7e2031fd
Christopher J. Brody <chris.brody@gmail.com>
2018-08-30 13:29:17 -0400

    Cleanup SyrButton.m
    - Fix @TODO & other comments
    - remove trailing spaces

--- ios/SyrNative/SyrNative/SyrButton.m
+++ ios/SyrNative/SyrNative/SyrButton.m
@@ -37,55 +37,62 @@ SYR_EXPORT_MODULE(Button)
 +(NSObject*) render: (NSDictionary*) component withInstance: (NSObject*) componentInstance  {
   SyrButtonView *button;
   NSString* guid = [[component objectForKey:@"instance"] valueForKey:@"uuid"];
-  
+
   if(componentInstance != nil) {
     button = (SyrButtonView*)componentInstance;
   } else {
     button = [[SyrButtonView alloc] init];
+
     SEL selector = NSSelectorFromString(@"handleSingleTap:");
-    //[button addTarget:[[SyrEventHandler sharedInstance] assignDelegate:guid] action:selector forControlEvents:UIControlEventTouchUpInside];
-      
-      //@TODO: going the touchable opacity route for now. Will need to come back and look at how to do this on a UIButton without subClassing it.
+
+    // [button addTarget:[[SyrEventHandler sharedInstance] assignDelegate:guid]
+    //            action:selector
+    //  forControlEvents:UIControlEventTouchUpInside];
+
+    // @TODO: going the touchable opacity route for now.
+    // Will need to come back and look at how to do this
+    // on a UIButton without subClassing it.
     SyrEventDelegate* eventHandler = [[SyrEventHandler sharedInstance] assignDelegate:guid];
     UITapGestureRecognizer *singleFingerTap = [[UITapGestureRecognizer alloc] initWithTarget:eventHandler action:selector];
     singleFingerTap.cancelsTouchesInView = NO;
     [button addGestureRecognizer:singleFingerTap];
   }
-  
+
   NSDictionary* style = [[component objectForKey:@"instance"] valueForKey:@"style"];
   NSDictionary* props = [[component objectForKey:@"instance"] valueForKey:@"props"];
   NSString* buttonTitle =  [[component objectForKey:@"instance"] valueForKey:@"value"];
-  
-	// default to button enabled
+
+  // default to button enabled
   id isEnabled = [[[component objectForKey:@"instance"] objectForKey:@"props"] objectForKey:@"enabled"];
   if(isEnabled != nil) {
     if([isEnabled boolValue] == NO) {
       button.enabled = false;
     }
   }
-    
+
     NSString* hapticFeedbackType = [props objectForKey:@"hapticFeedbackType"];
     if(hapticFeedbackType != nil) {
         button.feedbackType = hapticFeedbackType;
     }
-  
+
   NSString* titleColor = [style valueForKey:@"color"];
   if(titleColor != nil) {
     [button setTitleColor:[SyrStyler colorFromHash:titleColor] forState:UIControlStateNormal];
   }
-  
+
   [button setTitle:buttonTitle forState:UIControlStateNormal];
   button.frame = [SyrStyler styleFrame:style];
-  
+
   NSNumber* fontSize = [style valueForKey:@"fontSize"];
   if(fontSize != nil) {
    button.titleLabel.font = [UIFont boldSystemFontOfSize:[fontSize doubleValue]];
   }
 
-  //[button sizeToFit];
+  // [button sizeToFit];
+
   NSNumber* tag = [[component valueForKey:@"instance"] valueForKey:@"tag"];
   button.tag = [tag integerValue];
-  
+
   // Add an action in current code file (i.e. target)
   return (UIButton*)[SyrStyler styleView:button withStyle:style];
 }
