48f79af4b9431b7f21cccf94243ffa469a137962
sridharrajs <me@sridhar.co>
2019-02-18 15:46:43 +0530

    Added Subtract, Multiply and Divide functionalities to the calculator

--- samples/styles/calculator.js
+++ samples/styles/calculator.js
@@ -6,15 +6,17 @@ const Styles = {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
     backgroundColor: '#aaaaaa',
+    padding: 5
   },
-  buttons: {
-    width: PixelRatio.getPixelSizeForLayoutSize(600),
-    height: PixelRatio.getPixelSizeForLayoutSize(600),
+  buttonContainer: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
     top: PixelRatio.getPixelSizeForLayoutSize(200),
+    margin: 5
   },
   numButton: function(number) {
-    const row = Math.floor(number / 3) - (number % 3 ? 0 : 1);
-    const col = number % 3 ? number % 3 : 3; // 0, 1, 2
+    const row = Math.floor(number / 4) - (number % 4 ? 0 : 1);
+    const col = number % 4 ? number % 4 : 4; // 0, 1, 2
     const width = Dimensions.get('window').width / 5;
     const height = Dimensions.get('window').height / 7;
     const padding = PixelRatio.getPixelSizeForLayoutSize(10);
@@ -23,35 +25,24 @@ const Styles = {
     const top = row * height + padding * row;
 
     return {
+      fontSize: 20,
+      fontWeight: 'bold',
       height: height,
       width: width,
-      backgroundColor: '#eeeeee',
-      color: '#000000',
       left: left,
-      top: top,
+      backgroundColor: number % 4 === 0 ? '#f49b42' : '#eeeeee',
+      top: top
     };
   },
   displayArea: {
     textAlign: 'right',
     height: PixelRatio.getPixelSizeForLayoutSize(125),
     top: PixelRatio.getPixelSizeForLayoutSize(50),
-    left: PixelRatio.getPixelSizeForLayoutSize(10),
-    width:
-      Dimensions.get('window').width - PixelRatio.getPixelSizeForLayoutSize(20),
+    left: PixelRatio.getPixelSizeForLayoutSize(20),
+    width: Dimensions.get('window').width - PixelRatio.getPixelSizeForLayoutSize(30),
     backgroundColor: '#dedede',
-    fontSize: PixelRatio.getPixelSizeForLayoutSize(105),
-  },
-  plus: {
-    width: PixelRatio.getPixelSizeForLayoutSize(200),
-    height: PixelRatio.getPixelSizeForLayoutSize(200),
-    backgroundColor: '#f49b42',
-    fontSize: PixelRatio.getPixelSizeForLayoutSize(60),
-    left:
-      Dimensions.get('window').width -
-      PixelRatio.getPixelSizeForLayoutSize(200) -
-      PixelRatio.getPixelSizeForLayoutSize(10),
-    top: PixelRatio.getPixelSizeForLayoutSize(200),
-  },
+    fontSize: PixelRatio.getPixelSizeForLayoutSize(105)
+  }
 };
 
 export { Styles };
